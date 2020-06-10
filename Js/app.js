0e9ee4fe0d8b0be55cb06554edaaae7eafd51c3b
LouisPruvost <63111685+LouisPruvost@users.noreply.github.com>
2020-06-10 17:53:36 +0200

    corrections

--- Js/app.js
+++ Js/app.js
@@ -13,7 +13,7 @@ sr.reveal('#logo',{
     origin: 'top',
     distance: '50px',
     scale: '0.5',
-    duration: 5000, /* 1000 = 1 seconde */
+    duration: 1000, /* 1000 = 1 seconde */
     mobile: 'true', /* Pour le responsive */
 });
 
